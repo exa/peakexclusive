e2c627e79dfd5f7c0b7a07f6688c8a2289735cdf
Sergey Kuleshov <sergey@kuleshov.online>
2023-02-05 14:38:18 +0200

    testing contact form

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,7 +3,7 @@ import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest = mailchannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "Test", email: "svyatogor@gmail.com" }],
+      to: [{ name: "Test", email: "sergey@kuleshov.online" }],
     },
   ],
   from: { name: "Enquiry", email: "no-reply@peakexclusive.com" },
