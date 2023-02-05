1ed49e0f3ffbda58419c1fd53febb5fc09933b58
Sergey Kuleshov <sergey@kuleshov.online>
2023-02-05 15:23:12 +0200

    added thank you page

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,10 +3,10 @@ import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest = mailchannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "Test", email: "svyatogor@gmail.com" }],
+      to: [{ name: "Sergey K", email: "svyatogor@me.com" }],
     },
   ],
-  from: { name: "Enquiry", email: "no-reply@peakexclusive.com" },
+  from: { name: "Enquiry", email: "svyatogor@gmail.com" },
   subject: "New enquiry from Peak Exclusive",
   respondWith: () =>
     new Response(null, {
